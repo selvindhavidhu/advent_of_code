162b351e298fc19841a0f2b8dca9aeac4ae18d58
Selvin Dhavidhu <selvin.dhavidhu@gmail.com>
2024-11-30 22:42:25 +0530

    Added PowerShell Script to format the source files using clang-format

---
+++ format_files.ps1
@@ -0,0 +1,16 @@
+# Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+
+# Define the current directory as the root directory
+$rootDir = Get-Location
+
+# Recursively get all .c and .h files
+$files = Get-ChildItem -Path $rootDir -Recurse -Include *.c, *.h
+
+# Iterate over each file and run clang-format
+foreach ($file in $files) {
+    Write-Host "Formatting $($file.FullName)..."
+    clang-format -i $file.FullName
+    Write-Host "$($file.FullName) formatted."
+}
+
+Write-Host "Formatting completed."
