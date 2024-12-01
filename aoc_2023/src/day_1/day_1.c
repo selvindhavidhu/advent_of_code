eb232c74a202ae8c39646498b885c4bcdd244b49
Selvin Dhavidhu <selvin.dhavidhu@gmail.com>
2024-12-01 18:20:03 +0530

    daily commit 01-12-24

--- aoc_2023/src/day_1/day_1.c
+++ aoc_2023/src/day_1/day_1.c
@@ -54,15 +54,18 @@ int solve_day_1_part_2(const char *input_file_path)
 		exit(ENOENT);
 	}
 
-	const char words[9][6] = { "one", "two",   "three", "four", "five",
+	const char words[][6] = { "one", "two",   "three", "four", "five",
 				   "six", "seven", "eight", "nine" };
 
-	const int words_length[9] = {3, 3, 5, 4, 4, 3, 5, 5, 4};
+	const int words_length[] = { 3, 3, 5, 4, 4, 3, 5, 5, 4 };
 	int sum = 0;
 	char line[MAX_LINE_SIZE + 1] = { 0 };
 	int num[2] = { 0, 0 };
+	int a = 1;
 
 	while (fgets(line, MAX_LINE_SIZE, fp) != NULL) {
+		printf("line: %d\t", a++);
+		num[0] = num[1] = 0;
 		int line_length = (int)strlen(line);
 
 		/* Find the first digit. */
@@ -70,20 +73,45 @@ int solve_day_1_part_2(const char *input_file_path)
 			int ch = line[i];
 			if (isdigit(ch)) {
 				num[0] = ch - '0';
-				break;
+				goto search_second_digit;
 			} else {
-				
+				int len = (int) strlen(line + i);
+				for (int k = 0; k < 9; ++k) {
+					if (len >= words_length[k]) {
+						char const* pos = strstr(line + i, words[k]);
+						if (pos) {
+							num[0] = k + 1;
+							goto search_second_digit;
+						}
+					}
+				}
 			}
 		}
+
+		search_second_digit:
 
 		/* Find the second digit. */
 		for (int j = line_length - 1; j >= 0; --j) {
 			int ch = line[j];
 			if (isdigit(ch)) {
 				num[1] = ch - '0';
-				break;
+				goto end_of_search;
+			} else {
+				int len = (int) strlen(line + j);
+				for (int k = 0; k < 9; ++k) {
+					if (len >= words_length[k]) {
+						char const* pos = strstr(line + j, words[k]);
+						if (pos) {
+							num[1] = k + 1;
+							goto end_of_search;
+						}
+					}
+				}
 			}
 		}
+
+		end_of_search:
+		printf("num[0] = %d\t num[1] = %d\n", num[0], num[1]);
 
 		sum += (num[0] * 10) + num[1];
 		memset(line, 0, MAX_LINE_SIZE + 1);
