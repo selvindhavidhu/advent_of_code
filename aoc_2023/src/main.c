b0baf4c4ffd231f8b511022387610730e6d3d0d2
Selvin Dhavidhu <selvin.dhavidhu@gmail.com>
2024-12-04 01:33:58 +0530

    Changed code style to Microsoft

--- aoc_2023/src/main.c
+++ aoc_2023/src/main.c
@@ -1,10 +1,10 @@
-#include "stdheaders.h"
 #include "day_1/day_1.h"
+#include "stdheaders.h"
 
 int main(void)
 {
-	const char input_file_path[] = "2023_day_1_input.txt";
-	printf("output: %d\n", solve_day_1_part_2(input_file_path));
+    const char input_file_path[] = "2023_day_1_input.txt";
+    printf("output: %d\n", solve_day_1_part_2(input_file_path));
 
-	return 0;
+    return 0;
 }
